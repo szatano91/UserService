a885fc8335395dd5b1a5bf02dfc8277ea296ed43
Szatano <szatano.jakub@gmail.com>
2019-01-26 15:47:18 +0100

    fiftth commit

--- App.java
+++ App.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        User user = new User("Jakub", "Stoch", "12345", "jakub.stoch");
-        User user1 = new User("Adam", "Zyła", "12345", "adam.zyla");
-        User user2 = new User("Wojtek", "Kot", "12345", "wojtek.kot");
-
-        UserService nowy = new UserService();
-
-        nowy.addUser(user);
-        nowy.addUser(user1);
-        nowy.addUser(user2);
-        nowy.printAll();
+        Dictionary engDictionary = new Dictionary();
+        engDictionary.addTranslation("Piłka", "Ball");
+        engDictionary.addTranslation("laptop", "laptop");
+        engDictionary.addTranslation("telefon", "phone");
+        engDictionary.addTranslation("kubek", "cup");
+        engDictionary.addTranslation("kubek", "cup");
 
 
     }
--- Dictionary.java
+++ Dictionary.java
@@ -1,4 +1,38 @@
 package SDA;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Dictionary {
+
+    private Map<String, String> dictionary = new HashMap<>();
+
+    public void addTranslation(String pl, String trans) {
+
+        if (dictionary.containsKey(pl)) {
+            System.out.println("słowo juz istnieje");
+
+        } else dictionary.put(pl, trans);
+    }
+
+    public void edit(String pl, String trans) {
+
+        if (dictionary.containsKey(pl)) {
+            dictionary.put(pl, trans);
+        } else {
+            System.out.println("nie mozesz edytowac");
+        }
+    }
+
+    public void remove (String pl){
+
+    }
+
+    public String getTranslation(String pl) {
+        return dictionary.get(pl);
+    }
+
 }
+
